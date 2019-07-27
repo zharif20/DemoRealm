a7f87be012e3af867e60f747639fa819ba00162f
Zharif.Hadi @ 123RF <zharif.hadi@123rf.com>
2019-07-27 23:22:25 +0800

    Initial Commit

---
+++ DemoRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoRealm
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 27/07/2019.
+//  Copyright © 2019 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
